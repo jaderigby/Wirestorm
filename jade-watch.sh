006ddcc5b91b8d88c54b809b97ca0c9d063c9490
Jade Rigby <contact@jaderigby.com>
2023-05-25 22:37:40 -0600

    deleting old files; going to phase out wirestorm2 and just have wirestorm, period

--- jade-watch.sh
+++
@@ -1,3 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
-jade -w __jade__/app/ -o html/ --pretty
--- stylus-watch.sh
+++
@@ -1,4 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
-
-stylus -u jeet -u rupture -u nib -w __stylus__/main.styl -o css/
